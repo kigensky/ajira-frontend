6368d632adbce08336cb9d650fc83a7656fd7eb3
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-15 19:02:10 +0300

    update

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ListComponent } from './list.component';
+import { LoginComponent } from './login.component';
+import { SignupComponent } from './signup.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'list', component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
