635e00d3a3187158871083b1aef780f31bfef1ff
shamuu829 <shamsodarwish@gmail.com>
2021-06-17 09:56:55 +0300

    add all files

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
