a34fa3eb18518f0e910bbad9280eb17f6fb467c9
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-18 04:10:51 +0300

    update

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from './auth.service';
 import { ListComponent } from './list.component';
 import { LoginComponent } from './login.component';
 import { SignupComponent } from './signup.component';
@@ -10,11 +9,11 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'list', component: ListComponent, canActivate: [AuthGuard] },
+  { path: 'list', component: ListComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- project/src/app/list.component.ts
+++ project/src/app/list.component.ts
@@ -1,43 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ApiService } from './api.service';
-import { Employees } from './employees.interface';
+import { ShoppingItem } from './shopping-item.interface';
 
 @Component({
   selector: 'app-list',
   template: `
-  <div class="body" style="background-image: url(../assets/bc.jpeg)">  
-    <div style="text-align:center">
-      <h1>
-        Employee
-      </h1>
-    </div>
-    
-  </div>`
+  <div style="text-align:center">
+    <h1>
+      Lista de compras
+    </h1>
+  </div>
+  <ul>
+    <li *ngFor="let item of items">
+      <h2>{{ item.quantity }}x {{ item.name }}
+      <button (click)="delete(item.id)">x</button></h2>
+    </li>
+  </ul>
+
+  <input #itemQuantity type='text' placeholder='Qtd'>
+  <input #itemName type='text' placeholder='Name'>
+  <button (click)="add(itemName.value, itemQuantity.value)">Add</button>
+  <p>{{ error?.message }}</p>
+  <p *ngIf="error">{{ error?.error | json }}</p>
+
+  <button (click)="logout()">deslogar</button>
+  `
 })
 export class ListComponent implements OnInit {
 
-  items: Employees[];
+  items: ShoppingItem[];
   error: any;
 
   constructor(private api: ApiService) { }
 
   ngOnInit() {
-    this.api.getEmployees().subscribe(
-      (items: Employees[]) => this.items = items,
+    this.api.getShoppingItems().subscribe(
+      (items: ShoppingItem[]) => this.items = items,
       (error: any) => this.error = error
     );
   }
 
-  add(employeesName: string, employeesDepartment: number ) {
-    this.api. Employees(employeesName, employeesDepartment).employees(
-      (name: Employees) => this.items.push(name),
+  add(itemName: string, itemQuantity: number) {
+    this.api.createShoppingItem(itemName, itemQuantity).subscribe(
+      (item: ShoppingItem) => this.items.push(item),
       (error: any) => this.error = error
     );
   }
 
   delete(id: number) {
-    this.api.deleteEmployees(id).subscribe(
+    this.api.deleteShoppingItem(id).subscribe(
       (success: any) => this.items.splice(
         this.items.findIndex(item => item.id === id)
       ),
@@ -45,7 +57,3 @@ export class ListComponent implements OnInit {
     );
   }
 }
-function employeesName(employeesName: any, employeeDepartment: any) {
-  throw new Error('Function not implemented.');
-}
-
