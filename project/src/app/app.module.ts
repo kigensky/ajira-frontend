a3930b6a1a52664a3c51de343a394b974a719c97
victor kigen <vickigen@gmail.com>
2021-06-21 23:01:40 +0300

    added forms

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LeaveFormComponent } from './leave-form/leave-form.component';
+import { EmployeeSalaryComponent } from './employee-salary/employee-salary.component';
+import { PostComponent } from './post/post.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
 
 
 
@@ -12,7 +19,14 @@ import { NavbarComponent } from './navbar/navbar.component';
   declarations: [
     AppComponent,
     EmployeeFormComponent,
-    NavbarComponent, 
+    NavbarComponent,
+    LeaveFormComponent,
+    EmployeeSalaryComponent,
+    PostComponent,
+    HomepageComponent,
+    SignupComponent,
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
