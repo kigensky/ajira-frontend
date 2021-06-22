3c0fa2a7aac1e64b2c8f10e70be9f33484ab0865
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-22 21:41:44 +0300

    update

--- project/src/app/navbar/navbar.component.ts
+++ project/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Emitters } from '../emitters/emitters';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  authenticated = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    Emitters.authEmitter.subscribe(
+      (auth: boolean) => {
+        this.authenticated = auth;
+      }
+    );
+  }
+
+  logout(): void {
+    this.http.post('http://localhost:8000/api/logout', {}, {withCredentials: true})
+      .subscribe(() => this.authenticated = false);
   }
 
 }
+
