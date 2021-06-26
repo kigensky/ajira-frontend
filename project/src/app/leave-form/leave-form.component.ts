aaa66f5fb3b1989677b89f39df6ad220d7e1ff98
shamuu829 <shamsodarwish@gmail.com>
2021-06-26 22:03:01 +0300

    update leave form

--- project/src/app/leave-form/leave-form.component.ts
+++ project/src/app/leave-form/leave-form.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-leave-form',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leave-form.component.css']
 })
 export class LeaveFormComponent implements OnInit {
+  form!:FormGroup
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+
+    this.form =new FormGroup({
+      employee_name:new FormControl('', [Validators.required]),
+      department:new FormControl('', [Validators.required]),
+      month:new FormControl('', [Validators.required]),
+      year:new FormControl('', [Validators.required]),
+      start_date:new FormControl('', [Validators.required]),
+      end_date:new FormControl('', [Validators.required]),
+
+
+    });
+
+
   }
+
+  submit(){
+  
+    let url= "http://127.0.0.1:8000/api/leave/"
+    this.http.post(url, this.form.getRawValue(),{withCredentials:true}).subscribe( 
+    response => {console.log(response)}
+    
+    ,
+    error => {console.log(error)}
+    
+    
+    )
+    // console.log(this.form.getRawValue())
+  
+}
 
 }
