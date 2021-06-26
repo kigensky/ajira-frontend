5d3eeb6b3215e4e49f0ef0f68452f812a595f944
shamuu829 <shamsodarwish@gmail.com>
2021-06-26 21:29:18 +0300

    add salary form

--- project/src/app/employee-salary/employee-salary.component.ts
+++ project/src/app/employee-salary/employee-salary.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-employee-salary',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-salary.component.css']
 })
 export class EmployeeSalaryComponent implements OnInit {
+  form!:FormGroup
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+
+    this.form =new FormGroup({
+      employee_name:new FormControl('', [Validators.required]),
+      salary_date:new FormControl('', [Validators.required]),
+      salary_amount:new FormControl('', [Validators.required]),
+
+    });
   }
+
+  submit(){
+  
+    let url= "http://127.0.0.1:8000/api/employee_salary/"
+    this.http.post(url, this.form.getRawValue(),{withCredentials:true}).subscribe( 
+    response => {console.log(response)}
+    
+    ,
+    error => {console.log(error)}
+    
+    
+    )
+    // console.log(this.form.getRawValue())
+  
+}
+
 
 }
